6aad14ec8643ab357b3e1a8bfd93f548712a6beb
Guruprasad Ajegaonkar <68501124+GuruprasadAjegaonkar@users.noreply.github.com>
2025-02-20 23:23:01 +0530

    Add files via upload

---
+++ DETS-1/DETS/includes/dbconnection.php
@@ -0,0 +1,7 @@
+<?php
+$con=mysqli_connect("sql203.epizy.com", "epiz_31877958", "sFLOWcsGuw3LvqD", "epiz_31877958_detsdb");
+if(mysqli_connect_errno()){
+echo "Connection Fail".mysqli_connect_error();
+}
+
+  ?>
---
+++ DETS-1/DETS/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="col-sm-12">
+				<p class="back-link">©Daily Expense Tracker 2023</p>
+			</div>
